f7f70e8890f39eb64e4b682fecc0a7f2dc0ec18a
klma13 <47507965+klma13@users.noreply.github.com>
2019-04-10 21:32:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*for (i = 0; i < 2197; i++) {
+function napsatLi(){for (i = 0; i < 2197; i++) {
 	document.getElementById('demo').innerHTML += 'li';
-}*/
+}
+		   }		    
 document.getElementById("btn").addEventListener("click", napsatLI); 
